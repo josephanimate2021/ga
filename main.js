051bce61e1a20692a80d5765394437873895be92
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-21 16:48:25 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+// vars
+const http = require('http');
 
+const hostname = '127.0.0.1';
+const port = 80;
+
+// basic utilities
+const theme = require("./theme/list");
+const pages = require("./pages");
+const url = require('url');
+
+const utiltiies = {
+  theme,
+  pages
+};
+
+const server = http.createServer((req, res) => {
+  const purl = url.parse(req.url, true);
+  const found = utiltiies.find(u => u(req, res, purl));
+  if (found) {
+    res.statusCode = 200;
+    res.setHeader("Location", "/studio");
+  } else {
+    res.statusCode = 404;
+    res.end('404 not found');
+  }
+});
+
+server.listen(port, hostname, () => console.log(`Server running at http://${hostname}:${port}/`));
+  
