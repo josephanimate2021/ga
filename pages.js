5abe6a458268209c82fdd27faf04ec99214d97f7
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-21 18:31:19 -0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,4 +1,5 @@
 module.exports = function (req, res, url) {
+  if (req.method != "GET") return;
   switch (url.pathname) {
     case "/studio": {
       res.setHeader("Content-Type", "text/html; charset=utf8");
