fc3596eb98494c782b74c62bfaea925226eca387
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-21 19:19:11 -0400

    Update get.js

--- req/get.js
+++ req/get.js
@@ -8,10 +8,10 @@ const http = require("https");
 module.exports = function (url, options = {}) {
 	var data = [];
 	return new Promise((res, rej) => {
-    try {
-      https.get(url, options, (o) => o.on("data", (v) => data.push(v)).on("end", () => res(Buffer.concat(data))).on("error", rej));
-    } catch (e) {
-      http.get(url, options, (o) => o.on("data", (v) => data.push(v)).on("end", () => res(Buffer.concat(data))).on("error", rej));
-    }
+		try {
+			https.get(url, options, (o) => o.on("data", (v) => data.push(v)).on("end", () => res(Buffer.concat(data))).on("error", rej));
+		} catch (e) {
+			http.get(url, options, (o) => o.on("data", (v) => data.push(v)).on("end", () => res(Buffer.concat(data))).on("error", rej));
+		}
 	});
 };
