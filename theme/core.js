4f20cfca9c29c57b3315344efe550abc6fbec984
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-21 23:34:48 -0400

    Update core.js

--- theme/core.js
+++ theme/core.js
@@ -1,15 +1,19 @@
 const JSZip = require('jszip');
 const fs = require('fs');
 const loadPost = require('../req/body');
+const file = './files/themes/list.zip';
 
 module.exports = function (req, res) {
   if (req.method != "POST") return;
   switch (req.url) {
     case "/goapi/getThemeList/": {
-      fs.createReadStream('./files/themes/list.zip').pipe(res);
+      if (fs.existsSync(file)) fs.createReadStream(file).pipe(res);
+      else {console.log}
       return true;
     } case "/goapi/getTheme/": {
-      loadPost(req, res).then(data => fs.createReadStream(`./files/themes/${data.themeId}.zip`).pipe(res));
+      loadPost(req, res).then(data => {
+        if (fs.existsSync(file.slice(0, -8 + `${data.themeId}.zip`))) fs.createReadStream(file.slice(0, -8 + `${data.themeId}.zip`)).pipe(res));
+        else {console.log}
       return true;
     }
   }
