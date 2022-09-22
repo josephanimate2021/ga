bc8fdb4644a15fb2d996f79aacc06917b8f57172
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-22 05:58:49 -0400

    Update fileUtil.js

--- fileUtil.js
+++ fileUtil.js
@@ -1,3 +1,6 @@
+const JSZip = require('jszip');
+const fs = require('fs');
+
 module.exports = {
   makeid(length) {
     var result = '';
@@ -5,5 +8,20 @@ module.exports = {
     var charactersLength = characters.length;
     for ( var i = 0; i < length; i++ ) result += characters.charAt(Math.floor(Math.random() * charactersLength));
     return result;
+  },
+  makeZip(filename) {
+    const zip = new JSZip();
+
+    try {
+      const buffer = fs.readFileSync(filename);
+      zip.file(filename.slice(0, -3 + "zip"), buffer);
+
+      zip.generateNodeStream({ type: 'base64', streamFiles: true }).pipe(fs.createWriteStream(filename.slice(0, -3 + "zip"))).on('finish', function () {
+        return `${filename.slice(0, -3 + "zip")} written.`;
+      });
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
   }
 }
