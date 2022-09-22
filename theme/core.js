365e24b1c0008c32686006b92a064513b1272b25
josephanimate2021 <jackiecrosman@gmail.com>
2022-09-21 23:21:01 -0400

    fixed bugs with theme searching

--- theme/core.js
+++ theme/core.js
@@ -6,49 +6,11 @@ module.exports = function (req, res) {
   if (req.method != "POST") return;
   switch (req.url) {
     case "/goapi/getThemeList/": {
-      const zip = new JSZip();
-
-      try {
-        const buffer = fs.readFileSync('./files/themes/list.xml');
-        fs.writeFileSync("themelist.xml", buffer);
-        zip.file("themelist.xml", buffer);
-
-        zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true }).pipe(fs.createWriteStream('themelist.zip'));
-        fs.createReadStream('themelist.zip').pipe(res);
-      } catch (err) {
-        console.error(err)
-      }
+      fs.createReadStream('./files/themes/list.zip').pipe(res);
       return true;
     } case "/goapi/getTheme/": {
-      if (fs.existsSync('themelist.zip')) fs.unlinkSync('themelist.zip');
-      if (fs.existsSync('themelist.xml')) fs.unlinkSync('themelist.xml');
-      const zip = new JSZip();
-      loadPost(req, res).then(data => {
-        const tId = data.themeId;
-        if (fs.existsSync(`${tId}.zip`)) fs.unlinkSync(`${tId}.zip`);
-        if (fs.existsSync(`${tId}.xml`)) fs.unlinkSync(`${tId}.xml`);
-        fs.writeFileSync(process.env.DATABASES_FOLDER + `/themeData.json`, JSON.stringify(data));
-
-        try {
-          const buffer = fs.readFileSync(`./files/themes/${tId}.xml`);
-          fs.writeFileSync(`${tId}.xml`, buffer);
-          zip.file(`${tId}.xml`, buffer);
-
-          zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true }).pipe(fs.createWriteStream(`${tId}.zip`));
-          fs.createReadStream(`${tId}.zip`).pipe(res);
-        } catch (err) {
-          console.error(err)
-        }
-      });
+      loadPost(req, res).then(data => fs.createReadStream(`./files/themes/${data.themeId}.zip`).pipe(res));
       return true;
-    } case "/goapi/getUserWatermarks/": {
-      if (fs.existsSync(process.env.DATABASES_FOLDER + '/themeData.json')) {
-        const data = require('.' + process.env.DATABASES_FOLDER + '/themeData');
-        const tId = data.themeId;
-        if (!fs.existsSync(`${tId}.zip`) && !fs.existsSync(`${tId}.xml`)) return;
-        fs.unlinkSync(`${tId}.zip`);
-        fs.unlinkSync(`${tId}.xml`);
-      }
     }
   }
 };
