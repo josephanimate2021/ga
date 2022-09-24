c96fbf659cb73c766928b9cfd98ee0d197a1d6f5
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-23 22:58:40 -0400

    Update update.bat

--- update.bat
+++ update.bat
@@ -2,5 +2,7 @@
 if exist .git (
         echo Updating...
         git pull || git stash && git pull || git add . && git stash && git pull
+        echo Update Complete!
+        pause
         cls
-) else ( echo Git was not found. skipping update... )
+) else ( echo Git was not found. skipping update... && pause )
