e068ad51dd8b5b02e37789e1f917cdfa0663af22
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-24 17:42:14 -0400

    Update core.js

--- asset/core.js
+++ asset/core.js
@@ -3,6 +3,7 @@ const loadPost = require("../req/body");
 const asset = require('./main');
 const fUtil = require('../fileUtil');
 const fs = require('fs');
+const xml = require('../xml');
 // functions
 // server functions
 module.exports = function (req, res) {
@@ -46,6 +47,15 @@ module.exports = function (req, res) {
             fs.writeFileSync(process.env.DATABASES_FOLDER + `/tags/${id}.txt`, "");
             res.end(0 + id);
           }).catch(e => console.log(e));
+          return true;
+        } case "/goapi/getCcCharCompositionXml/": {
+          loadPost(req, res).then(data => asset.loadCharacter(data.assetId || data.original_asset_id)).then(b => {
+            if (!b) res.end(1 + xml.error());
+            else res.end(Buffer.from(b));
+          }).catch(e => {
+            res.end(1 + xml.error(e));
+            console.log(e);
+          });
         }
       }
     }
