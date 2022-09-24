19877f7bbe6c487be8e7b80b4da503e65798909d
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-09-24 17:33:04 -0400

    Update fileUtil.js

--- fileUtil.js
+++ fileUtil.js
@@ -10,6 +10,9 @@ module.exports = {
     for ( var i = 0; i < length; i++ ) result += characters.charAt(Math.floor(Math.random() * charactersLength));
     return result;
   },
+  fileString(filepath) {
+    return filepath;
+  },
   exists(filepath) {
     return fs.existsSync(filepath);
   },
