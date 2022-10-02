47973f49cbf39fdcec9d16f3576ec6d096b5fd6c
josephanimate2021 <jackiecrosman@gmail.com>
2022-10-02 18:32:00 -0400

    fixed node env detection not working

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 set NODE_ENV=dev
-if !NODE_ENV!==dev ( call update.bat )
+if %NODE_ENV%==dev ( call update.bat )
 if not exist node_modules ( npm install && npm start && npm error ) else ( npm start && npm error )
