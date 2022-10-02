97dac8f17b83af6bc70b4796608dfe768ee1c57a
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-10-02 00:20:33 -0400

    Delete main.js

--- static/main.js
+++
@@ -1,8 +0,0 @@
-const get = require("../req/get");
-const env = require("../env");
-
-module.exports = {
-  getAnimationSwfs(name) {
-    return new Promise((res, rej) => get(env.SWF_URL + `/${name}.swf`).then(b => res(b)).catch(e => rej(e)));
-  },
-};
