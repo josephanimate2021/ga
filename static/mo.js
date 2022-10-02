29a2c7e539a9dbd50d166d47562e73fd90272f5e
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-10-02 00:26:12 -0400

    Create mo.js

---
+++ static/mo.js
@@ -0,0 +1,15 @@
+const get = require("../req/get");
+const env = require("../env");
+
+module.exports = function (req, res, url) {
+  if (req.method != "GET") return;
+  const match = req.url.match(/\/static\/client_theme\/locale\/go\/en_US\/([^.]+)(?:\.mo)?$/);
+  if (!match) return;
+  const id = match[1];
+  const ext = match[2];
+  get(env.CLIENT_URL + `/client_theme/locale/go/en_US/${id}.${ext}`).then(b => res.end(b)).catch(e => {
+    console.log(e); 
+    res.end('404 Not Found'); 
+  });
+  return true;
+}
