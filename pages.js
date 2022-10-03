5f7d6489007f5bb211d9a9f6d41bc47cc094d18e
josephanimate2021 <jackiecrosman@gmail.com>
2022-10-02 23:52:08 -0400

    bug fixes

--- pages.js
+++ pages.js
@@ -93,31 +93,30 @@ module.exports = function (req, res, url) {
         <br><h1>Your Movies</h1><br>${files.map(v => `${v.html}`).join('') || '<h2>You currently have no movies right now. <button id="createBtn">Create one now</button></h2></center>'}
         <script>
         // Get the modal
-        var modal = document.getElementById("myModal");
-        
-        // Get the button that opens the modal
-        var btn = document.getElementById("myBtn");
-        var createBtn = document.getElementById("createBtn");
-        var charBtn = document.getElementById("char");
-        
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
-        
-        // When the user clicks the button, open the modal 
-        btn.onclick = function() {
-          modal.style.display = "block";
-        }
-        createBtn.onclick = function() {
-          modal.style.display = "block";
-        }
-        charBtn.onclick = function() {
-          location.href = '/charcreator?themeId=family&bs=adam';
-        }
-        
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-          modal.style.display = "none";
-        }
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
         </script>
         </body>
         </html>`;
