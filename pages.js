1630393c8010faa7c7679d6e62ae0c9a63aa3664
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-10-11 18:00:51 -0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -8,7 +8,9 @@ aniClientUrl = env.CLIENT_URL;
 
 module.exports = function (req, res, url) {
   if (req.method != "GET") return;
-  var html, tId;
+  var html, tId, mId;
+  if (url.query.movieId) mId = `&amp;movieId=${url.query.movieId}`;
+  else mId = '';
   const urlPrefix = req.headers.host == "localhost" ? "http" : req.headers.host == `localhost:${process.env.port}` ? "http" : "https";
   switch (url.pathname) {
    case "/": {
@@ -16,7 +18,7 @@ module.exports = function (req, res, url) {
         break;
     } case "/player": {
 	    res.setHeader("Content-Type", "text/html; charset=utf8");
-	    html = `<body margin=0px"><embed height="100%" flashvars="apiurl=%2Fmovie%2Fassets%2Fapi.xml&amp;asseturl=%2Fmovie%2Fassets&amp;baseurl=${urlPrefix}://${req.headers.host}l&amp;lang=en-local&amp;userid=416896&amp;playhelpmovie=1" pluginspage="http://www.adobe.com/go/getflashplayer" src="${aniSwfUrl}/player.swf" type="application/x-shockwave-flash" width="100%"></embed></body>`;
+	    html = `<body margin=0px"><embed height="100%" flashvars="apiurl=%2Fmovie%2Fassets%2Fapi.xml&amp;asseturl=%2Fmovie%2Fassets&amp;baseurl=${urlPrefix}://${req.headers.host}l&amp;lang=en-local&amp;userid=416896${mId}" pluginspage="http://www.adobe.com/go/getflashplayer" src="${aniSwfUrl}/player.swf" type="application/x-shockwave-flash" width="100%"></embed></body>`;
 	    break;
     } case "/studio": {
 	    res.setHeader("Content-Type", "text/html; charset=utf8");
