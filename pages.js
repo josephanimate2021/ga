d6cd59cc4721c113827e6d812647567403cdb4b7
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-10-13 07:27:29 -0400

    Update pages.js

--- pages.js
+++ pages.js
@@ -77,7 +77,7 @@ module.exports = function (req, res, url) {
                 
                         <ul id="nav"><li class="menu-343 first"><a href="/" id="nav-home">Home</a></li>
         <li class="menu-345"><a href="/movie/watch" id="nav-watch">Watch A Movie</a></li>
-        <li class="menu-344"><a href="/studio" id="nav-make">Make A Movie</a></li>
+        <li class="menu-344"><a href="/studio" onclick="alert('Because of the way this server was designed, the old legacy movie starters wont be added. All movies will be created on a blank stage.')" id="nav-make">Make A Movie</a></li>
         <li class="menu-920"><a href="/spotlight" id="nav-spotlight">Spotlight</a></li>
         <li class="menu-347"><a href="/extras" id="nav-extras">Extras</a></li>
         <li class="menu-79 last"><a href="/help" id="nav-help">Help</a></li>
