5c9480ecb967eb0eff175b96cd6e739e1c6c58c3
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-10-13 06:35:32 -0400

    Update main.js

--- movie/main.js
+++ movie/main.js
@@ -4,7 +4,8 @@ module.exports = {
     listMovies() {
         const table = [];
         fs.readdirSync(process.env.MOVIE_FOLDER).forEach(file => {
-            if (fs.existsSync(process.env.MOVIE_FOLDER + `/${file}`)) table.unshift({html: `<li class="movie-clip clear-block"><a href="/player?movieId=${file.slice(0, -4)}" class="thumbnail"><img src="https://web.archive.org/web/20120915212352im_/http://www.zimmertwins.com/sites/zimmertwins.com/movie/thumbnails/small/edgar_faints_living_room.png" alt="Test" title="Test"/></a><!-- Movie Clip Types --><h2><a href="/player?movieId=${file.slice(0, -4)}">Test</a></h2></li>`});
+            const id = file.slice(0, -4);
+            if (fs.existsSync(process.env.MOVIE_FOLDER + `/${file}`)) table.unshift({html: `<li class="movie-clip clear-block"><a href="/player?movieId=${id}" class="thumbnail"><img src="https://web.archive.org/web/20120915212352im_/http://www.zimmertwins.com/sites/zimmertwins.com/movie/thumbnails/small/edgar_faints_living_room.png" alt="Test" title="Test"/></a><!-- Movie Clip Types --><h2><a href="/player?movieId=${id}">Test</a></h2></li>`});
         });
         return table;
     }
