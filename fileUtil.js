3d33a1248b7329a567d65dc7f175906f6188442e
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-10-13 22:35:38 -0400

    Update fileUtil.js

--- fileUtil.js
+++ fileUtil.js
@@ -1,5 +1,3 @@
-const JSZip = require('jszip');
-const zip = new JSZip;
 const fs = require('fs');
 
 module.exports = {
@@ -15,39 +13,5 @@ module.exports = {
   },
   exists(filepath) {
     return fs.existsSync(filepath);
-  },
-  zipList() {
-    return new Promise((res, rej) => {
-      try {
-        const buffer = fs.readFileSync('./files/themes/list.xml');
-        fs.writeFileSync("themelist.xml", buffer);
-        zip.file("themelist.xml", buffer);
-  
-        zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true }).pipe(fs.createWriteStream('themelist.zip')).on('finish', function () {
-          res("themelist.zip written.");
-        });
-      } catch (err) {
-        rej(err);
-      }
-    });
-  },
-  addToZip(zip, zipName, buffer) {
-		zip.add(zipName, buffer);
-		if (zip[zipName].crc32 < 0) zip[zipName].crc32 += 4294967296;
-	},
-  zipTheme(tId) {
-    return new Promise((res, rej) => {
-      try {
-        const buffer = fs.readFileSync(`./files/themes/${tId}.xml`);
-        fs.writeFileSync("theme.xml", buffer);
-        zip.file("theme.xml", buffer);
-  
-        zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true }).pipe(fs.createWriteStream('theme.zip')).on('finish', function () {
-          res("theme.zip written.");
-        });
-      } catch (err) {
-        rej(err);
-      }
-    });
   }
 }
