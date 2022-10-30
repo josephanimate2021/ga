a0ec0217232dcc12a8a5375960a85eeea0cd8ed1
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-10-29 22:47:47 -0400

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 @echo off
-set NODE_ENV=dev
+:: env config
+if exist .git ( set NODE_ENV=dev ) else ( set NODE_ENV=production )
+:: where everything else begins.
 if %NODE_ENV%==dev ( call update.bat )
 if not exist node_modules ( npm install && npm start ) else ( npm start )
