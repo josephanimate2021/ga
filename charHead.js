67443e68321552436019e50359bcb767a3be6a3f
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-11-07 01:44:02 -0500

    pirate works now

--- charHead.js
+++ charHead.js
@@ -2,10 +2,12 @@ const fs = require("fs");
 
 module.exports = function (req, res, url) {
 	if (req.method != "GET") return;
-	const match = req.url.match(/\/char_heads\/([^/]+)$/);
+	const match = req.url.match(/\/char_heads\/([^/]+)([^.]+)(?:\.(png|jpg))?$/);
 	if (!match) return;
-	const file = match[1];
-	const pathName = process.env.ASSETS_FOLDER + `/charHeads/${file}`;
+	const tId = match[1];
+	const id = match[2];
+	const ext = match[3];
+	const pathName = process.env.ASSETS_FOLDER + `/charHeads/${tId}/${id}.${ext}`;
 	const html = fs.existsSync(pathName) ? fs.readFileSync(pathName) : `404 Not Found`;
 	res.end(html);
 	return fs.existsSync(pathName) ? true : "";
