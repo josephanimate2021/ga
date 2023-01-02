1e90f8b9945305a09aff3f705011f7c4b93fb097
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2023-01-01 19:46:06 -0800

    bug fixes after a while.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // vars
 const http = require('http');
+const fs = require('fs');
 
 const env = {
   hostname: '127.0.0.1',
@@ -24,26 +25,9 @@ env.SOUNDS_FOLDER = `${sFolder}/effects`;
 env.VOICEOVERS_FOLDER = `${sFolder}/voiceovers`;
 env.CHARS_FOLDER = `${folder}/chars`;
 env.DATABASES_FOLDER = `${folder}/meta`;
-
-// basic utilities
-const theme = require("./theme/core");
-const movie = require("./movie/core");
-const asset = require("./asset/core");
-const pages = require("./pages");
-const crossdomain = require("./crossdomain");
-const url = require('url');
-const fs = require('fs');
-
-const utilities = [
-  movie,
-  asset,
-  theme,
-  crossdomain,
-  pages
-];
-
 fs.writeFileSync(`./env.json`, JSON.stringify(env));
 if (!fs.existsSync(folder)) fs.mkdirSync(folder);
+if (!fs.existsSync(sFolder)) fs.mkdirSync(sFolder);
 if (!fs.existsSync(env.MOVIE_FOLDER)) fs.mkdirSync(env.MOVIE_FOLDER);
 if (!fs.existsSync(env.STARTER_FOLDER)) fs.mkdirSync(env.STARTER_FOLDER);
 if (!fs.existsSync(env.BG_FOLDER)) fs.mkdirSync(env.BG_FOLDER);
@@ -53,6 +37,23 @@ if (!fs.existsSync(env.MUSIC_FOLDER)) fs.mkdirSync(env.MUSIC_FOLDER);
 if (!fs.existsSync(env.VOICEOVERS_FOLDER)) fs.mkdirSync(env.VOICEOVERS_FOLDER);
 if (!fs.existsSync(env.CHARS_FOLDER)) fs.mkdirSync(env.CHARS_FOLDER);
 if (!fs.existsSync(env.DATABASES_FOLDER)) fs.mkdirSync(env.DATABASES_FOLDER);
+
+// basic utilities
+const theme = require("./theme/core");
+const movie = require("./movie/core");
+const asset = require("./asset/core");
+const pages = require("./pages");
+const crossdomain = require("./crossdomain");
+const url = require('url');
+
+const utilities = [
+  movie,
+  asset,
+  theme,
+  crossdomain,
+  pages
+];
+
 Object.assign(process.env, require("./env"));
 const server = http.createServer((req, res) => {
   try {
@@ -70,4 +71,3 @@ server.listen(env.port, env.hostname, () => {
   if (env.port == 80) console.log(`Server running at http://localhost/`);
   else console.log(`Server running at http://localhost:${env.port}/`);
 });
-  
