6c0d591b653abd15571e6937175e5183e039ee97
Joseph <100003036+josephanimate2021@users.noreply.github.com>
2023-06-24 12:53:25 -0400

    Update core.js

--- tts/core.js
+++ tts/core.js
@@ -5,79 +5,9 @@ const ffmpeg = require("fluent-ffmpeg");
 const tempfile = require("tempfile");
 ffmpeg.setFfmpegPath(require("@ffmpeg-installer/ffmpeg").path);
 const fs = require("fs");
-function handleError(e) { // handles the error for both the ternimal and the lvm
-    console.log(e);
-    return 1 + `<error><code>ERR_ASSET_404</code><message>${e}</message><text></text></error>`;
-}
 const fUtil = require("../fileUtil");
 const mp3Duration = require("mp3-duration");
-function sampleUrl(id) {
-    return new Promise((res, rej) => {
-        https.get(`https://api.uberduck.ai/voices/${id}/samples`, (r) => {
-            let buffers = [];
-            r.on("data", (b) => buffers.push(b)).on("end", () => {
-                try {
-                    const json = JSON.parse(Buffer.concat(buffers));
-                    if (json[0]) res(json[0].url);
-                    else res("");
-                } catch (e) {
-                    rej(e);
-                }
-            }).on("error", rej);
-        }).on("error", rej);
-    });
-}
-function duration(data) {
-    return new Promise((res, rej) => {
-        mp3Duration(data, (e, duration) => {
-            if (e || !duration) {
-                return rej(e || "Unable to retreive buffer duration");
-            }
-            const dur = duration * 1e3;
-            res(dur);
-        });
-    });
-}
-function convertToMp3(data, fileExt) {
-    return new Promise((res, rej) => {
-        const command = ffmpeg(data)
-            .inputFormat(fileExt)
-            .toFormat("mp3")
-            .audioBitrate(4.4e4)
-            .on("error", (err) => rej(err));
-        res(command.pipe());
-    });
-}
-function checkSpeakStatus(id) {
-    return new Promise((res, rej) => {
-        https.get(`https://api.uberduck.ai/speak-status?uuid=${id}`, (r) => {
-            let buffers = [];
-            r.on("data", (b) => buffers.push(b)).on("end", () => {
-                try {
-                    const json = JSON.parse(Buffer.concat(buffers));
-                    res(json);
-                } catch (e) {
-                    rej(e);
-                }
-            }).on("error", rej);
-        }).on("error", rej);
-    });
-}
-function voiceDetail(id) {
-    return new Promise((res, rej) => {
-        https.get(`https://api.uberduck.ai/voices/${id}/detail`, (r) => {
-            let buffers = [];
-            r.on("data", (b) => buffers.push(b)).on("end", () => {
-                try {
-                    const json = JSON.parse(Buffer.concat(buffers));
-                    res(json);
-                } catch (e) {
-                    rej(e);
-                }
-            }).on("error", rej);
-        }).on("error", rej);
-    });
-}
+const tts = require("./main");
 let xml;
 let xmlSucess = false;
 https.get('https://api.uberduck.ai/voices?mode=tts-basic&language=english&is_commercial=false&is_private=false&slim=false', (r) => {
@@ -87,7 +17,7 @@ https.get('https://api.uberduck.ai/voices?mode=tts-basic&language=english&is_com
             const json = JSON.parse(Buffer.concat(buffers));
             for (const voice of json) {
                 xmlSucess = true;
-                xml += `<voice id="${voice.voicemodel_uuid}" desc="${voice.display_name}" sex="M" demo-url="${await sampleUrl(voice.voicemodel_uuid)}" country="US" plus="N"/>`;
+                xml += `<voice id="${voice.voicemodel_uuid}" desc="${voice.display_name}" sex="M" demo-url="${await tts.sampleUrl(voice.voicemodel_uuid)}" country="US" plus="N"/>`;
             }
         } catch (e) {
             console.log(e);
@@ -104,7 +34,7 @@ module.exports = function(req, res, url) {
             break;
         } case "/goapi/convertTextToSoundAsset/": {
             loadPost(req, res).then(async data => {
-                const voice = await voiceDetail(data.voice);
+                const voice = await tts.voiceDetail(data.voice);
                 request({
                     method: 'POST',
                     url: 'https://api.uberduck.ai/speak',
@@ -120,15 +50,15 @@ module.exports = function(req, res, url) {
                     json: true
                 }, async (error, response, body) => {
                     if (error) return res.end(handleError(error));
-                    let json = await checkSpeakStatus(body.uuid);
-                    while (json.path === null) json = await checkSpeakStatus(body.uuid);
-                    https.get(json.path, (r) => convertToMp3(r, "wav").then(buff => {
+                    let json = await tts.checkSpeakStatus(body.uuid);
+                    while (json.path === null) json = await tts.checkSpeakStatus(body.uuid);
+                    https.get(json.path, (r) => tts.convertToMp3(r, "wav").then(buff => {
                         let buffers = [];
                         buff.on("data", (b) => buffers.push(b)).on("end", async () => {
                             const buffer = Buffer.concat(buffers);
                             const id = `${fUtil.makeid(12)}.mp3`;
                             const title = `[${voice.display_name}] ${data.text}`;
-                            const dur = await duration(buffer);
+                            const dur = await tts.duration(buffer);
                             if (!fs.existsSync('./sounds')) fs.mkdirSync('./sounds');
                             fs.writeFileSync(`./sounds/${id}`, buffer);
                             res.end(`0<response><asset><id>${id}</id><enc_asset_id>${id}</enc_asset_id><type>sound</type><subtype>tts</subtype><title>${title}</title><published>0</published><tags></tags><duration>${dur}</duration><downloadtype>progressive</downloadtype><file>${id}</file></asset></response>`);
